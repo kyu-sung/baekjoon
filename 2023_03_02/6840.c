2ed40d7a1312ff8caf6262e31215a33d8492a357
kyu-sung <kkskks119@naver.com>
2023-03-02 16:57:58 +0900

    2023_03_02

---
+++ 2023_03_02/6840.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c;
+
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
+
+	if (a > b && a < c)
+		printf("%d\n", a);
+	if (a < b && a > c)
+		printf("%d\n", a);
+	if (b > a && b < c)
+		printf("%d\n", b);
+	if (b < a && b > c)
+		printf("%d\n", b);
+	if (c > a && c < b)
+		printf("%d\n", c);
+	if (c < a && c > b)
+		printf("%d\n", c);
+	return (0);
+}
