b0aa6c440438f562e0e53503f7a57ae76ab199d5
kyu-sung <kkskks119@naver.com>
2023-03-09 22:45:25 +0900

    2023_03_09

---
+++ 2023_03_09/2446.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j, N, k;
+
+	scanf("%d", &N);
+	i = N;
+	while (i > 0)
+	{
+		k = i;
+		while (k < N)
+		{
+			printf(" ");
+			k++;
+		}
+		j = 0;
+		while (j < i * 2 - 1)
+		{
+			printf("*");
+			j++;
+		}
+		printf("\n");
+		i--;
+	}
+	i = i + 2;
+	while (i <= N)
+	{
+		k = i;
+		while (k < N)
+		{
+			printf(" ");
+			k++;
+		}
+		j = 0;
+		while (j < i * 2 - 1)
+		{
+			printf("*");
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+	return (0);
+}
